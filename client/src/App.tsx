51b4cea19ab390d03a6f64c549e552b674405389
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-09-05 18:50:39 +0530

    feat: enhance Home page layout with styling and title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 const App = () => {
     return (
         <Router>
-            <div className="w-screen h-screen bg-black flex justify-center items-center"></div>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/:shortUrl" element={<Redirect />} />
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="flex justify-center bg-black   items-center w-screen h-screen text-white">
+            <h1 className="text-4xl">Url Shortner</h1>
+        </div>
+    );
 };
 
 export default Home;
