96829942aaa0eb17ee7b5d384acb3756c4203b2a
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-09-07 23:31:59 +0530

    feat: add theme toggle functionality and dropdown menu
    - Implemented ModeToggle component for switching between light, dark, and system themes.
    - Created ThemeProvider to manage theme state and persist it in local storage.
    - Added DropdownMenu component with various items for theme selection.
    - Wrapped the main App component with ThemeProvider in main.tsx.
    - Enhanced Home page with input and button for URL shortening feature.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const Home = () => {
     return (
         <div className="flex justify-center bg-black flex-col  items-center w-screen h-screen text-white gap-4">
             <h1 className="text-4xl">Url Shortner</h1>
-            <div className="flex w-full max-w-sm items-center gap-2"></div>
+            <div className="flex w-full max-w-sm items-center gap-2">
+                <div className="flex w-full max-w-sm items-center gap-2">
+                    <Input type="text" placeholder="Website Url" />
+                    <Button type="submit" variant="outline">
+                        Short
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
